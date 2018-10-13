6d523cc69483539119d13b23a3603e9965c5234b
PkcPrashant <pc22230@gmail.com>
2018-10-14 01:21:02 +0530

    Initial Commit

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,6 +4,7 @@ export default function(state = [], action) {
 	switch (action.type) {
 		case FETCH_WEATHER:
 			return [ action.payload.data, ...state ];       //state.concat([action.payload.data]);
+		default:
+			return state;
 	}
-	return state;
 }
